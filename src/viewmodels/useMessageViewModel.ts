49c5c0cbffd66e84a8cc7b6ff4673607a6c627c4
akhr-ymd <akhr-ymd@outlook.com>
2026-01-12 17:45:14 +0900

    入力文字数制限

--- src/viewmodels/useMessageViewModel.ts
+++ src/viewmodels/useMessageViewModel.ts
@@ -1,5 +1,7 @@
-import { computed } from "vue";
+import { computed, watch } from "vue";
 import { useMessageModel } from "../models/messageModel"
+
+const MAX_LENGTH = 10;
 
 export function useMessageViewModel() {
     // Model
@@ -8,6 +10,13 @@ export function useMessageViewModel() {
     // View 用に加工
     const messageLength = computed(() => {
         return messageModel.message.value.length
+    })
+
+    // 入力監視
+    watch(messageModel.message, (newMessage) => {
+        if (newMessage.length > MAX_LENGTH) {
+            messageModel.message.value = newMessage.slice(0, MAX_LENGTH)
+        }
     })
 
     // View から呼ばれる操作
